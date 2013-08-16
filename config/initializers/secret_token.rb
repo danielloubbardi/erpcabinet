66a7d171e0022564b1d7ab7592e976d576d24ad7
danielloubbardi <daniel.loubbardi@gmail.com>
2013-08-16 03:15:03 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Erpcabinet::Application.config.secret_key_base = '8552f9181d9cc3d461bec9f9a2f26d7b2d538a6fa4106da76dec7353c0cd2c2091b0d681a8f0a590a3b18ef0722dbc74aceedf0513f46ea2a4bf392191b1b387'
