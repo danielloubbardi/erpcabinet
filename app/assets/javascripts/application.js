8469896e364ffc198e69deb34313eaac096e59f4
danielloubbardi <daniel.loubbardi@gmail.com>
2013-08-31 21:16:49 +0200

    a

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,62 @@
 // require jquery
 // require jquery_ujs
 //= require turbolinks
-//= require_tree .
+// require_tree .
 
 // Loads all Bootstrap javascripts
-// require bootstrap
+// bootstrap require
+
+
+
+//=require js/libs/jquery.min
+//=require js/libs/jquery.ui.min
+//=require js/include/jquery.ui.touch-punch.min
+//=require js/include/jquery.accordion.min
+//=require js/include/toastr.min
+//=require js/include/jquery.jgrowl.min
+//=require js/include/slimScroll.min
+//=require js/include/jquery.dataTables.min
+//=require js/include/DT_bootstrap.min
+
+// ici mettre les fichiers si mobile ou pas
+
+//=require js/include/jquery.ibutton.min
+//=require js/include/justgage.min
+//=require js/include/raphael.2.1.0.min
+//=require js/include/jquery.easy-pie-chart.min
+//=require js/include/jarvis.widget.min
+//=require js/include/mobiledevices.min
+// require js/include/json2
+//=require js/include/jquery.fullcalendar.min
+//=require js/include/jquery.flot.cust.min
+//=require js/include/jquery.flot.resize.min
+//=require js/include/jquery.flot.tooltip.min
+//=require js/include/jquery.flot.orderBar.min	
+//=require js/include/jquery.flot.fillbetween.min	
+//=require js/include/jquery.flot.pie.min
+//=require js/include/jquery.sparkline.min
+
+//=require js/include/jquery.inbox.slashc.sliding-menu.min
+//=require js/include/jquery.validate.min
+//=require js/include/bootstrap-progressbar.min
+//=require js/include/wysihtml5-0.3.0.min
+//=require js/include/bootstrap-wysihtml5.min
+//=require js/include/jquery.maskedinput.min
+//=require js/include/bootstrap-datepicker.min
+//=require js/include/bootstrap.wizard.min
+//=require js/include/bootstrap-colorpicker.min
+//=require js/include/bootstrap-timepicker.min   
+//=require js/include/bootbox.min
+//=require js/include/bootstrap.min
+//=require js/config
+//=require js/include/d3.v3.min
+//=require js/include/adv_charts/d3-chart-1
+//=require js/include/adv_charts/d3-chart-2
+//=require js/include/adv_charts/geochart
+
+
+
+
+
+
+
